799a6c325bd8cfdb05a69b62f8f95cccbd152f45
JVandead19 <elfreyder@gmail.com>
2023-11-18 23:26:33 -0400

    p

--- Proyect/settings.py
+++ Proyect/settings.py
@@ -90,10 +90,7 @@ WSGI_APPLICATION = 'Proyect.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default = 'postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
